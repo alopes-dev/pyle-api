8599f694254a7778c2b2b6d3f040e26a3de7e1b8
Antonio Lopes <pdkdidy2011@gmail.com>
2025-01-05 19:02:34 +0000

    chore: gitigore

--- prisma/migrations/20250105190208_user/migration.sql
+++ prisma/migrations/20250105190208_user/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone_number" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
